7949ad5ece7c2bd2f77ad354d97098114183fe48
zwb <1546841551@qq.com>
2022-06-24 17:00:13 +0800

    新建

---
+++ src/main/java/com/atguigu/Gitee.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author zwb
+ * 创建时间:2022/6/24 16:55
+ * @ClassName:gittest
+ */
+public class Gitee {
+    public static void main(String[] args) {
+        System.out.println("新建");
+    }
+}
